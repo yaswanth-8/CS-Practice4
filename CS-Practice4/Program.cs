483b5d896cabbac79221a92d012ad0580b6d23bc
yaswanth <ya8wanth@gmail.com>
2023-03-28 09:32:55 +0530

    Add project files.

---
+++ CS-Practice4/Program.cs
@@ -0,0 +1,10 @@
+﻿using CS_Practice4;
+
+namespace MyProject;
+class Program
+{
+    static void Main(string[] args)
+    {
+        delegates del = new delegates();
+    }
+}
---
+++ CS-Practice4/delegates.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Practice4
+{
+    delegate void DoSum(int x, int y);
+    internal class delegates
+    {
+        public delegates (){
+
+            DoSum add = Sum;
+            add(2,6);
+        }
+
+        public static void Sum(int x, int y)
+        {
+            Console.WriteLine("Sum is "+( x + y));
+        }
+    }
+   
+}
